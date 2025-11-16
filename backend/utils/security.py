ae326a50addefaa52939f4787a95c10b735650e2
SIAG SOFTWARE <siag.software@protonmail.com>
2025-11-16 15:06:08 -0300

    Create security.py

---
+++ backend/utils/security.py
@@ -0,0 +1,9 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(pw: str):
+    return pwd_context.hash(pw)
+
+def verify_password(plain: str, hashed: str):
+    return pwd_context.verify(plain, hashed)
